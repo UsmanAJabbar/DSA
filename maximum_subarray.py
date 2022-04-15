986ffcd9f8efa14e5b6081f01e438bf6268bb025
UsmanAJabbar <hi@usmanjabbar.com>
2022-04-15 12:40:54 -0500

    Add buy sell stock

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,13 +1,20 @@
-def maximum_subarray(nums):
-    subarray_max = float('-inf')
+def max_subarray(nums):
+    subarray_sum = 0
     global_max = nums[0]
 
-    for curr_num in nums:
-        subarray_max = max(
-            curr_num,
-            subarray_max + curr_num
-        )
+    for n in nums:
+        subarray_sum += n
 
-        global_max = max(global_max, subarray_max)
-    
+        if subarray_sum > global_max:
+            global_max = subarray_sum
+
+        if subarray_sum < 0:
+            subarray_sum = 0
+
     return global_max
+
+print(
+    max_subarray(
+        [-2,1,-3,4,-1,2,1,-5,4]
+    )
+)
