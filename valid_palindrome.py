6fd746e02f97881a7847dab01a744f862a1c7d90
UsmanAJabbar <hi@usmanjabbar.com>
2022-04-21 01:11:16 -0500

    Fix valid_palindrome

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -8,9 +8,9 @@ def is_palindrome(string: str) -> bool:
     l, r = 0, len(string) - 1
 
     while l < r:
-        while not is_valid_character(string[l]):
+        while l < r and not is_valid_character(string[l]):
             l += 1
-        while not is_valid_character(string[r]):
+        while l < r and not is_valid_character(string[r]):
             r -= 1
 
         if string[l].lower() != string[r].lower():
